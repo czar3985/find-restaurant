4248d44ee960952a0d24e2331aae5e6bc9183443
Rina Lingad <pixie.czar@gmail.com>
2018-09-27 07:11:22 +1200

    Implement the foursquare API request

--- findARestaurant.py
+++ findARestaurant.py
@@ -14,10 +14,28 @@ foursquare_client_secret = json.loads(open('client_secrets.json', 'r').
 
 
 def findARestaurant(mealType,location):
-	#1. Use getGeocodeLocation to get the latitude and longitude coordinates of the location string.
+	# Get the latitude and longitude coordinates of the location string
+    latitude, longitude = getGeocodeLocation(location)
 
-	#2.  Use foursquare API to find a nearby restaurant with the latitude, longitude, and mealType strings.
-	#HINT: format for url will be something like https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+	# Use foursquare API to find a nearby restaurant with the latitude,
+	# longitude, and mealType strings
+	# Build the URL using this format:
+	# https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&
+	# client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+    url = ('https://api.foursquare.com/v2/venues/search?'
+           'client_id=%s&'
+           'client_secret=%s&'
+           'v=20130815&'
+           'll=%s,%s&'
+           'query=%s'% (foursquare_client_id, foursquare_client_secret,
+                        latitude, longitude, mealType))
+
+    # Make an API request
+    h = httplib2.Http()
+    response, content = h.request(url,'GET')
+
+    # Process the response
+    result = json.loads(content)
 
 	#3. Grab the first restaurant
 	#4. Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
@@ -25,7 +43,7 @@ def findARestaurant(mealType,location):
 	#6. If no image is available, insert default a image url
 	#7. Return a dictionary containing the restaurant name, address, and image url
 
-    return
+    return result
 
 
 if __name__ == '__main__':
