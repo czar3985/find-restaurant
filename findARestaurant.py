7567e4f753f494035c37bb1ff80de8eb3ec8a408
Rina Lingad <pixie.czar@gmail.com>
2018-09-27 13:40:42 +1200

    Make an API request for Venue Photo or provide a default if none is available

--- findARestaurant.py
+++ findARestaurant.py
@@ -30,7 +30,7 @@ def findARestaurant(mealType,location):
            'query=%s'% (foursquare_client_id, foursquare_client_secret,
                         latitude, longitude, mealType))
 
-    # Make an API request
+    # Make an API request for Search
     h = httplib2.Http()
     response, content = h.request(url,'GET')
 
@@ -42,27 +42,40 @@ def findARestaurant(mealType,location):
     restaurant_address = (' ').join(result['response']['venues'][0]
                                     ['location']['formattedAddress'])
 
-    print("\nRestaurant Name: " + restaurant_name)
-    print("Restaurant Address: " + restaurant_address)
+    print('\nRestaurant Name: ' + restaurant_name)
+    print('Restaurant Address: ' + restaurant_address)
 
-	#4. Get a  300x300 picture of the restaurant using the venue_id (you can
-	#change this by altering the 300x300 value in the URL or replacing it with
-	#'orginal' to get the original picture
-	#5. Grab the first image
-	#6. If no image is available, insert default a image url
-	#7. Return a dictionary containing the restaurant name, address, and image
-	#url
+    # Make an API request for Venue photo
+    venue_id = result['response']['venues'][0]['id']
 
+    url = ('https://api.foursquare.com/v2/venues/%s/photos?'
+           'client_id=%s&'
+           'client_secret=%s&'
+           'v=20130815&'
+           'limit=1' % (venue_id, foursquare_client_id, foursquare_client_secret))
+    response, content = h.request(url,'GET')
+    result = json.loads(content)
+
+    # Get a  300x300 picture of the restaurant using the venue_id
+	# If no image is available, insert default a image url
+    if result['response']['photos']['count'] != 1:
+        image = ('https://cdn.pixabay.com/photo/2016/03/05/19/02/'
+                 'hamburger-1238246_960_720.jpg')
+    else:
+        image = (result['response']['photos']['items'][0]['prefix'] +
+                 '300x300' +
+                 result['response']['photos']['items'][0]['suffix'])
+    print('Image: ' + image)
     return
 
 
 if __name__ == '__main__':
-    findARestaurant("Pizza", "Tokyo, Japan")
-    findARestaurant("Tacos", "Jakarta, Indonesia")
-    findARestaurant("Tapas", "Maputo, Mozambique")
-    findARestaurant("Falafel", "Cairo, Egypt")
-    findARestaurant("Spaghetti", "New Delhi, India")
-    findARestaurant("Cappuccino", "Geneva, Switzerland")
-    findARestaurant("Sushi", "Los Angeles, California")
-    findARestaurant("Steak", "La Paz, Bolivia")
-    findARestaurant("Gyros", "Sydney Australia")
+    findARestaurant('Pizza', 'Tokyo, Japan')
+    findARestaurant('Tacos', 'Jakarta, Indonesia')
+    findARestaurant('Tapas', 'Maputo, Mozambique')
+    findARestaurant('Falafel', 'Cairo, Egypt')
+    findARestaurant('Spaghetti', 'New Delhi, India')
+    findARestaurant('Cappuccino', 'Geneva, Switzerland')
+    findARestaurant('Sushi', 'Los Angeles, California')
+    findARestaurant('Steak', 'La Paz, Bolivia')
+    findARestaurant('Gyros', 'Sydney Australia')
